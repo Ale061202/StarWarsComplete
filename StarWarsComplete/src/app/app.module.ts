f85529ebf76589fc64645bba7a3ce75e2d904a13
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-10-15 19:54:30 +0200

    adding films-list

--- StarWarsComplete/src/app/app.module.ts
+++ StarWarsComplete/src/app/app.module.ts
@@ -1,19 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialImportsModule } from './modules/material-imports.interface';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialImportsModule } from './modules/material-imports/material-imports.module';
-import { CharactersListComponent } from './components/characters-list/characters-list.component';
-import { FilmsListComponent } from './components/films-list/films-list.component';
-import { SpeciesListComponent } from './components/species-list/species-list.component';
+import { FilmsComponent } from './components/films-list/films-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CharactersListComponent,
-    FilmsListComponent,
-    SpeciesListComponent
+    FilmsComponent,
   ],
   imports: [
     BrowserModule,
