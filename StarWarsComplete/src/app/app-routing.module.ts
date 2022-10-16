29c4f302c9ff1bfc79e871a009d2053910f212c6
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-10-16 18:40:15 +0200

    adding app-module species-list

---
+++ StarWarsComplete/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SpeciesComponent } from './components/species-list/species-list.component';
+import { StarshipListComponent } from './components/starship-list/starship-list.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
+import { PlanetsListComponent } from './planets-list/planets-list.component';
+
+const routes: Routes = [
+  {path: 'planets-list', component: PlanetsListComponent,pathMatch:'full'},
+  {path: 'starship-list',component:StarshipListComponent,pathMatch:'full'},
+  {path: 'vehicles-list',component:VehiclesListComponent,pathMatch:'full'},
+  {path: 'species-list',component:SpeciesComponent,pathMatch:'full'},
+  {path: '',redirectTo:'planets-list',pathMatch:'full'}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
