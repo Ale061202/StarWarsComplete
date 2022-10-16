6a087df2f84d48b245ec7b14df1512ce85767686
ArturoCes <cespedes.peart@triana.salesianos.edu>
2022-10-16 13:45:50 +0200

    eo

--- StarWarsComplete/src/app/app.module.ts
+++ StarWarsComplete/src/app/app.module.ts
@@ -4,20 +4,22 @@ import { MaterialImportsModule } from './modules/material-imports.interface';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialImportsModule } from './modules/material-imports/material-imports.module';
 import { FilmsComponent } from './components/films-list/films-list.component';
 import { SpeciesComponent } from './components/species-list/species-list.component';
+import { StarshipListComponent } from './components/starship-list/starship-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmsComponent,
-    SpeciesComponent
+    SpeciesComponent,
+    StarshipListComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MaterialImportsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
