4ae8d92d8e62ee328d7754e402902073962fdcfe
ArturoCes <cespedes.peart@triana.salesianos.edu>
2022-10-16 13:28:55 +0200

    commit

--- StarWarsComplete/src/app/app.module.ts
+++ StarWarsComplete/src/app/app.module.ts
@@ -4,15 +4,16 @@ import { MaterialImportsModule } from './modules/material-imports.interface';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialImportsModule } from './modules/material-imports/material-imports.module';
 import { FilmsComponent } from './components/films-list/films-list.component';
 import { SpeciesComponent } from './components/species-list/species-list.component';
+import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmsComponent,
-    SpeciesComponent
+    SpeciesComponent,
+    VehiclesListComponent
   ],
   imports: [
     BrowserModule,
