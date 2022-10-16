f7b8088c93590522faa0ce7a678952029ec7bc3e
Alejandro <fernandez.goale21@triana.salesianos.edu>
2022-10-16 18:22:39 +0200

    configuring app-module.ts

--- StarWarsComplete/src/app/app.module.ts
+++ StarWarsComplete/src/app/app.module.ts
@@ -5,21 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StarshipListComponent } from './components/starship-list/starship-list.component';
-import { PlanetsListComponent } from './planets-list/planets-list.component';
-import { MaterialImportsModule } from './modules/material-imports/material-imports.module';
 import { FilmsComponent } from './components/films-list/films-list.component';
 import { SpeciesComponent } from './components/species-list/species-list.component';
 import { VehiclesListComponent } from './components/vehicles-list/vehicles-list.component';
 import { PlanetsListComponent } from './planets-list/planets-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialImportsModule } from './material-imports/materialimports';
+import { MaterialImportsModule } from './modules/material-imports.interface';
 import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlanetsListComponent
+    PlanetsListComponent,
     FilmsComponent,
     SpeciesComponent,
     StarshipListComponent,
@@ -30,7 +27,6 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule,
     MaterialImportsModule
   ],
   providers: [],
